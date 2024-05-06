5a819a1a513b55312a392687f1d8ab370d489778
koudailiyoutang <126934404+koudailiyoutang@users.noreply.github.com>
2024-05-06 22:01:12 +0800

    个人中心页面调整

--- src/components/tab/user/userCenter.vue
+++ src/components/tab/user/userCenter.vue
@@ -1,89 +1,118 @@
 <template>
-<div >
-  <div style="display: flex; justify-content: center; align-items: center; height: calc(100vh - 150px);">
-   <el-card style="width: 40%;">
-     <h3 style="text-align: center;margin-bottom: 20px;font-weight: bolder;">用户个人信息</h3>
-     <el-form ref="form" :model="form" label-width="150px" style="width: 60%;font-size: 15px" :rules="rules">
-       <el-form-item label="用户名:" prop="username" >
-         <el-input @blur="checkRepetition" v-model="form.username"></el-input>
-       </el-form-item>
-       <el-form-item label="角色:" >
-         <el-input v-model="form.role" disabled></el-input>
-       </el-form-item>
-       <el-form-item label="创建时间:">
-         <el-input  v-model="form.createTime" disabled></el-input>
-       </el-form-item>
-       <el-form-item label="密保问题1:" prop="answer1">
-         <el-input v-model="form.question1" disabled></el-input>
-       </el-form-item>
-       <el-form-item label="密保答案1:" prop="answer1">
-         <el-input v-model="form.answer1" ></el-input>
-       </el-form-item>
-       <el-form-item label="密保问题2:" prop="answer1">
-         <el-input v-model="form.question2" disabled></el-input>
-       </el-form-item>
-       <el-form-item label="密保答案2:" prop="answer2">
-         <el-input v-model="form.answer2"></el-input>
-       </el-form-item>
-       <el-form-item label="密保问题3:" prop="answer1">
-         <el-input v-model="form.question3" disabled></el-input>
-       </el-form-item>
-       <el-form-item label="密保答案3:" prop="answer3">
-         <el-input v-model="form.answer3"></el-input>
-       </el-form-item>
-       <el-form-item v-if="form.role === '普通用户'" label="剩余上传文件容量:">
-         <el-input  v-model="form.uploadSize" disabled></el-input>
-       </el-form-item>
-       <el-form-item>
-         <el-button type="primary" @click="submitForm">保存</el-button>
-       </el-form-item>
-     </el-form>
-
-   </el-card>
- </div>
-</div>
+  <div>
+    <el-popover placement="bottom-start" trigger="hover">
+      <div style="width: auto">返回首页</div>
+      <el-page-header
+        style="margin: 30px auto auto 50px"
+        @back="goBack"
+        content="用户信息页"
+        class="custom-page-header"
+        slot="reference"
+      ></el-page-header>
+    </el-popover>
+    <div
+      style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 150px);
+      "
+    >
+      <el-card style="width: 40%">
+        <h3
+          style="text-align: center; margin-bottom: 20px; font-weight: bolder"
+        >
+          用户个人信息
+        </h3>
+        <el-form
+          ref="form"
+          :model="form"
+          label-width="150px"
+          style="width: 70%; font-size: 15px; margin: 0 auto"
+          :rules="rules"
+        >
+          <el-form-item label="用户名:" prop="username">
+            <el-input
+              @blur="checkRepetition"
+              v-model="form.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="角色:">
+            <el-input v-model="form.role" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="创建时间:">
+            <el-input v-model="form.createTime" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="密保问题1:" prop="answer1">
+            <el-input v-model="form.question1" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="密保答案1:" prop="answer1">
+            <el-input v-model="form.answer1"></el-input>
+          </el-form-item>
+          <el-form-item label="密保问题2:" prop="answer1">
+            <el-input v-model="form.question2" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="密保答案2:" prop="answer2">
+            <el-input v-model="form.answer2"></el-input>
+          </el-form-item>
+          <el-form-item label="密保问题3:" prop="answer1">
+            <el-input v-model="form.question3" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="密保答案3:" prop="answer3">
+            <el-input v-model="form.answer3"></el-input>
+          </el-form-item>
+          <el-form-item
+            v-if="form.role === '普通用户'"
+            label="剩余上传文件容量:"
+          >
+            <el-input v-model="form.uploadSize" disabled></el-input>
+          </el-form-item>
+        </el-form>
+        <div class="button">
+          <el-button type="primary" @click="submitForm">保存</el-button>
+        </div>
+      </el-card>
+    </div>
+  </div>
 </template>
 
-<script >
-import {getRequest, postRequest} from "@/utils/api";
-
+<script>
+import { getRequest, postRequest } from "@/utils/api";
 
 export default {
-  name:"userCenter",
-  data(){
-    return{
-      form:{
-        username:'',
-        role:'',
-        createTime:'',
-        answer1:'',
-        answer2:'',
-        answer3:'',
-        question1:'',
-        question2:'',
-        question3:'',
-        uploadSize:'',
-        uid: sessionStorage.getItem("uid")
-            ? sessionStorage.getItem("uid")
-            : "",
+  name: "userCenter",
+  data() {
+    return {
+      form: {
+        username: "",
+        role: "",
+        createTime: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        question1: "",
+        question2: "",
+        question3: "",
+        uploadSize: "",
+        uid: sessionStorage.getItem("uid") ? sessionStorage.getItem("uid") : "",
       },
-      rules:{
+      rules: {
         username: [
           { required: true },
-          { min: 2, max: 8, message: '长度在 2 到 8 个字符', trigger: 'blur' }
+          { min: 2, max: 8, message: "长度在 2 到 8 个字符", trigger: "blur" },
         ],
-        answer1:[
+        answer1: [
           { required: true },
-          {min:1, max:20, message: '长度在 1到 20 个字符', trigger: 'blur'}
+          { min: 1, max: 20, message: "长度在 1到 20 个字符", trigger: "blur" },
         ],
-        answer2:[
+        answer2: [
           { required: true },
-          {min:1, max:20, message: '长度在 1到 20 个字符', trigger: 'blur'}
+          { min: 1, max: 20, message: "长度在 1到 20 个字符", trigger: "blur" },
         ],
-        answer3:[
+        answer3: [
           { required: true },
-          {min:1, max:20, message: '长度在 1到 20 个字符', trigger: 'blur'}
-        ]
+          { min: 1, max: 20, message: "长度在 1到 20 个字符", trigger: "blur" },
+        ],
       },
       questionOptions: [
         {
@@ -107,88 +136,96 @@ export default {
           label: "您出生在哪个城市？",
         },
       ],
-    }
+    };
   },
   created() {
-    this.getUserDetail()
+    this.getUserDetail();
   },
-  methods:{
-    getUserDetail(){
-      console.log("id:",sessionStorage.getItem("userid") - 0)
-      getRequest(`/user/getmessage/${sessionStorage.getItem("userid") - 0}`).then(res => {
-        if(res.code === 200){
-          console.log(res)
-          let data = res.data
-          this.form.username = data.username
-          if(data.role === 0){
-            this.form.role = "管理员"
-          }else if(data.role === 1){
-            this.form.role = "普通用户"
+  methods: {
+    getUserDetail() {
+      console.log("id:", sessionStorage.getItem("userid") - 0);
+      getRequest(
+        `/user/getmessage/${sessionStorage.getItem("userid") - 0}`
+      ).then((res) => {
+        if (res.code === 200) {
+          console.log(res);
+          let data = res.data;
+          this.form.username = data.username;
+          if (data.role === 0) {
+            this.form.role = "管理员";
+          } else if (data.role === 1) {
+            this.form.role = "普通用户";
           }
-          console.log(data)
-          const inputDateTime  = data.createTime
+          console.log(data);
+          const inputDateTime = data.createTime;
           const datePart = inputDateTime.split("T")[0];
-          this.form.createTime = datePart
-          this.form.question1 = data.answer1.split(":")[0]
-          this.form.question2 = data.answer2.split(":")[0]
-          this.form.question3 = data.answer3.split(":")[0]
-          this.form.answer1 = data.answer1.split(":")[1]
-          this.form.answer2 = data.answer2.split(":")[1]
-          this.form.answer3 = data.answer3.split(":")[1]
-          this.form.uploadSize = data.uploadSize
-          let test =  `${this.form.question1}:${this.form.answer1 }`;
-          console.log(test)
+          this.form.createTime = datePart;
+          this.form.question1 = data.answer1.split(":")[0];
+          this.form.question2 = data.answer2.split(":")[0];
+          this.form.question3 = data.answer3.split(":")[0];
+          this.form.answer1 = data.answer1.split(":")[1];
+          this.form.answer2 = data.answer2.split(":")[1];
+          this.form.answer3 = data.answer3.split(":")[1];
+          this.form.uploadSize = data.uploadSize;
+          let test = `${this.form.question1}:${this.form.answer1}`;
+          console.log(test);
         }
-      })
+      });
     },
-    checkRepetition(){
-        getRequest(`user/checkRepetition/${this.form.username}`).then(res=>{
-          if(res.code === 200){
-            if(res.msg === "用户名已存在"){
-              this.$message.error("用户名已存在，请重新输入")
-              this.getUserDetail()
-            }else {
-              return;
-            }
+    checkRepetition() {
+      getRequest(`user/checkRepetition/${this.form.username}`).then((res) => {
+        if (res.code === 200) {
+          if (res.msg === "用户名已存在") {
+            this.$message.error("用户名已存在，请重新输入");
+            this.getUserDetail();
+          } else {
+            return;
           }
-        })
+        }
+      });
     },
-    submitForm(){
+    goBack() {
+      this.$router.push({
+        path: "/sideBar/dash",
+      });
+    },
+    submitForm() {
       const form = {
-        username:'',
-        answer1:'',
-        answer2:'',
-        answer3:'',
-        uid: sessionStorage.getItem("uid")
-            ? sessionStorage.getItem("uid")
-            : "",
+        username: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        uid: sessionStorage.getItem("uid") ? sessionStorage.getItem("uid") : "",
+      };
+      if (this.form.role === "管理员") {
+        this.form.role = 0;
+      } else if (this.form.role === "普通用户") {
+        this.form.role = 1;
       }
-      if(this.form.role === '管理员'){
-        this.form.role = 0
-      }else if(this.form.role ==='普通用户'){
-        this.form.role = 1
-      }
-      form.username = this.form.username
-      form.answer1 =`${this.form.question1}:${this.form.answer1 }`
-      form.answer2 = `${this.form.question2}:${this.form.answer2 }`
-      form.answer3 = `${this.form.question3}:${this.form.answer3 }`
-      postRequest('/user/updateUser',form).then(res => {
-        if(res.code === 200){
+      form.username = this.form.username;
+      form.answer1 = `${this.form.question1}:${this.form.answer1}`;
+      form.answer2 = `${this.form.question2}:${this.form.answer2}`;
+      form.answer3 = `${this.form.question3}:${this.form.answer3}`;
+      postRequest("/user/updateUser", form).then((res) => {
+        if (res.code === 200) {
           window.sessionStorage.setItem("user", this.form.username);
-          this.getUserDetail()
-          this.$message.success("修改成功")
-          setTimeout(function() {
+          this.getUserDetail();
+          this.$message.success("修改成功");
+          setTimeout(function () {
             location.reload();
           }, 100);
-        }else {
-          this.$message.error("修改失败")
+        } else {
+          this.$message.error("修改失败");
         }
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+.button {
+  display: flex;
+  justify-content: center;
+}
 </style>
